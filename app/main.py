294396aeae1432b4a689bf1034ae41cc54518942
Ivan Synytskyi <ivansinickij29@icloud.com>
2026-02-27 10:24:20 +0100

    fixed flake8

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def format_linter_error(error: dict) -> dict:
 
-
     return {
         "line": error["line_number"],
         "column": error["column_number"],
@@ -10,9 +9,7 @@ def format_linter_error(error: dict) -> dict:
     }
 
 
-
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-
 
     return {
         "path": file_path,
@@ -21,13 +18,9 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     }
 
 
-
 def format_linter_report(linter_report: dict) -> list:
-
 
     return [
         format_single_linter_file(file_path, errors)
         for file_path, errors in linter_report.items()
     ]
-
-
